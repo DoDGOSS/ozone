f4d3626119ccf7acb2394352e625163a6d67a0cb
aeiseman <alexandra.eiseman@geocent.com>
2019-02-04 08:45:20 -0600

    adding CreateDashboardDialog

---
+++ ozone-framework-client/src/components/create-dashboard-screen/PremadeLayouts.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import { Field, FieldProps } from "formik";
+import { RadioGroup } from "@blueprintjs/core";
+import { DEFAULT_LAYOUTS, IMAGE_ROOT_URL } from "../../stores/LayoutStore";
+
+   const PremadeStyles={
+      paddingBottom:"15px",
+    };
+
+    export interface PremadeLayoutsProps {
+          onChange:(event: React.FormEvent<HTMLElement>) => void;
+          selectedValue?:string;
+    }
+
+    const _PremadeLayouts: React.FunctionComponent<PremadeLayoutsProps & FieldProps<any>> =
+
+        ({ onChange }) => {
+
+          return (
+                  <div style={PremadeStyles}>
+                  <RadioGroup onChange={onChange}>
+                    {DEFAULT_LAYOUTS.map(Layout =>
+                        <button className='layout' key={Layout.name} value={Layout.name} >
+                          <img src={IMAGE_ROOT_URL + Layout.iconUrl} />
+                        </button>
+                    )}
+                  </RadioGroup>
+                  </div>
+            );
+        };
+
+    _PremadeLayouts.displayName = "PremadeLayouts";
+
+
+    export const PremadeLayouts: React.FunctionComponent<PremadeLayoutsProps> = (props) => (
+        <Field component={_PremadeLayouts} {...props}/>
+    );
