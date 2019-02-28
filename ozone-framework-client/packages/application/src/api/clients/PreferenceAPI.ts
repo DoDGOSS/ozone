7049ffff581034f5b8f62c1c254473a866b30bbc
Chris Hill <hill.c.james@gmail.com>
2019-02-28 00:06:52 -0500

    add a preferences api, untested

--- ozone-framework-client/packages/application/src/api/clients/PreferenceAPI.ts
+++ ozone-framework-client/packages/application/src/api/clients/PreferenceAPI.ts
@@ -1,4 +1,17 @@
-import { Gateway, getGateway, Response } from "../interfaces";
+
+import {
+	Gateway,
+	getGateway,
+	Response
+} from "../interfaces";
+import {
+	PreferenceCreateResponse,
+	PreferenceDeleteResponse,
+	PreferenceUpdateResponse,
+	PreferenceCreateRequest,
+	PreferenceUpdateRequest
+} from "../models/PreferenceDTO";
+import * as qs from "qs";
 
 export class PreferenceAPI {
     private readonly gateway: Gateway;
@@ -21,6 +34,46 @@ export class PreferenceAPI {
 
     async getServerResources(): Promise<Response<any>> {
         return this.gateway.get(`prefs/server/resources/`);
+    }
+
+
+    async createPreference(data: PreferenceCreateRequest): Promise<Response<PreferenceCreateResponse>> {
+        const requestData = qs.stringify({
+            data: JSON.stringify([data])
+        });
+
+        return this.gateway.post(`prefs/preference/${data.namespace}/${data.path}/`, requestData, {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            validate: PreferenceCreateResponse.validate
+        });
+    }
+
+    async updatePreference(data: PreferenceUpdateRequest): Promise<Response<PreferenceUpdateResponse>> {
+        const requestData = qs.stringify({
+            data: JSON.stringify([data])
+        });
+
+        return this.gateway.put(`prefs/preference/${data.namespace}/${data.path}/`, requestData, {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            validate: PreferenceUpdateResponse.validate
+        });
+    }
+
+    async deletePreference(data: PreferenceCreateRequest): Promise<Response<PreferenceDeleteResponse>> {
+        const requestData = qs.stringify({
+            _method: "DELETE"
+        });
+
+        return this.gateway.post(`prefs/preference/${data.namespace}/${data.path}/`, requestData, {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            validate: PreferenceDeleteResponse.validate
+        });
     }
 }
 
---
+++ ozone-framework-client/packages/application/src/api/models/PreferenceDTO.ts
@@ -0,0 +1,91 @@
+import { createValidator, Model, Property } from "@ozone/openapi-decorators";
+
+@Model({ name: "Preference" })
+export class PreferenceDTO {
+
+    static validate = createValidator(PreferenceDTO);
+
+    @Property()
+    id: number;
+
+    @Property()
+    namespace: string;
+
+    @Property()
+    path: string;
+
+    @Property()
+    value: string;
+
+}
+
+
+@Model()
+export class PreferenceGetResponse {
+
+    static validate = createValidator(PreferenceGetResponse);
+
+    @Property()
+    results: number;
+
+    @Property(() => PreferenceDTO)
+    data: PreferenceDTO[];
+
+}
+
+
+export interface PreferenceCreateRequest {
+    namespace: string;
+    path: string;
+    value?: string;
+}
+
+
+@Model()
+export class PreferenceCreateResponse {
+
+    static validate = createValidator(PreferenceCreateResponse);
+
+    @Property()
+    success: boolean;
+
+    @Property(() => PreferenceDTO)
+    data: PreferenceDTO;
+
+}
+
+
+export interface PreferenceUpdateRequest extends PreferenceCreateRequest {
+  namespace: string;
+  path: string;
+  value?: string;
+
+}
+
+
+@Model()
+export class PreferenceUpdateResponse {
+
+    static validate = createValidator(PreferenceUpdateResponse);
+
+    @Property()
+    success: boolean;
+
+    @Property(() => PreferenceDTO)
+    data: PreferenceDTO;
+
+}
+
+
+@Model()
+export class PreferenceDeleteResponse {
+
+    static validate = createValidator(PreferenceDeleteResponse);
+
+    @Property()
+    success: boolean;
+
+    @Property(() => PreferenceDTO)
+    data: PreferenceDTO;
+
+}
