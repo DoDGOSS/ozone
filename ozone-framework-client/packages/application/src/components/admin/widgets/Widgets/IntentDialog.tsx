1dcf79577558158850cb681707c15fe4576577e1
Chris Hill <hill.c.james@gmail.com>
2019-04-03 17:31:07 -0400

    Almost working - widget setup now in its own component. Table exists for viewing/searching intents. Can create/edit/delete intents. Widgets can almost be edited; need to convert existing WidgetDTO into a format the WidgetCreateForm can understand. Just saving progress.

---
+++ ozone-framework-client/packages/application/src/components/admin/widgets/Widgets/IntentDialog.tsx
@@ -0,0 +1,59 @@
+import * as React from "react";
+import { Button, Dialog } from "@blueprintjs/core";
+import { Form, Formik, FormikActions, FormikProps } from "formik";
+import { array, boolean, number, object, string } from "yup";
+import * as uuidv4 from "uuid/v4";
+
+import { CancelButton, CheckBox, FormError, HiddenField, SelectField, SubmitButton, TextField } from "../../../form";
+import { widgetApi } from "../../../../api/clients/WidgetAPI";
+import { WidgetTypeReference } from "../../../../api/models/WidgetTypeDTO";
+import { Intent } from '../../../../models/Intent';
+import { IntentForm } from './IntentForm';
+
+import * as styles from "../Widgets.scss";
+
+interface Props {
+    isOpen: boolean;
+    onClose: () => void;
+    onSubmit: (e: any) => void;
+    intentToEdit?: Intent;
+}
+
+export class IntentDialog extends React.Component<Props, {}> {
+
+    constructor(props: Props) {
+        super(props);
+    }
+
+    render() {
+
+        return (
+        <Dialog
+            title={this.getTitle()}
+            className={styles.loginScreen}
+            isOpen={this.props.isOpen}
+            onClose={this.props.onClose}
+        >
+            <div>
+                <IntentForm
+                    intentToEdit={this.props.intentToEdit}
+                    onSubmit={(newI: any) => {
+                        this.props.onSubmit(newI);
+                        this.props.onClose()}
+                    }
+                />
+            </div>
+        </Dialog>);
+    }
+
+    private getTitle(): string {
+        if (this.props.intentToEdit) {
+            return 'Edit Intent';
+        }
+        return 'Create Intent';
+    }
+
+    private getTheme() {
+        return '';
+    }
+}
