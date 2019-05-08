26511b6055a1c2d8a452a9006dfd0568e928779a
Jeff Harlan <jeff.harlan@nextcentury.com>
2019-05-08 15:53:46 -0400

    build scripts to include ozone-framework-client production assets in owf-framework-server bundle
    Additional changes:
    * Embed Consent Notice and User Agreement in Login page (maybe be disabled by configuration)
    * Authentication status messages (logged out, session invalid, timeout, etc) to Login page
    * Update HTML (and GSP) templates to automatically include generated chunks and css assets
    * Global environment settings that can be provided at launch: server URLs, context path, asset path, etc.
    
    Related issue(s):
    DG-991

---
+++ ozone-framework-client/packages/application/src/environment/interfaces.ts
@@ -0,0 +1,46 @@
+declare global {
+    interface Window {
+        env: Environment;
+    }
+}
+
+export interface Environment {
+    server: ServerOptions;
+    login: LoginOptions;
+    logout: LogoutOptions;
+    consentNotice: ConsentNoticeOptions;
+    userAgreement: UserAgreementOptions;
+}
+
+export interface ServerOptions {
+    url: string;
+    contextPath: string;
+    staticAssetPath: string;
+}
+
+export interface LoginOptions {
+    isEnabled: boolean;
+    loginUrl: string;
+    nextUrl: string;
+}
+
+export interface LogoutOptions {
+    isEnabled: boolean;
+    logoutUrl: string;
+}
+
+export interface ConsentNoticeOptions {
+    isEnabled?: boolean;
+    title: string;
+    message: string;
+    details: {
+        isEnabled: boolean;
+        linkText: string;
+    };
+    nextUrl?: string;
+}
+
+export interface UserAgreementOptions {
+    title: string;
+    message: string;
+}
