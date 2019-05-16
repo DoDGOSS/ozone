10f6b0b3fd2247d263aa1ac639d3bb064751fd0b
aeiseman <alie.eiseman@geocent.com>
2019-05-16 09:47:58 -0500

    removing title from copy of layout

--- ozone-framework-client/packages/application/src/stores/default-layouts.ts
+++ ozone-framework-client/packages/application/src/stores/default-layouts.ts
@@ -93,16 +93,12 @@ export async function createPresetLayout(layoutName: string | null, guid: string
 
 const onCopyDashboard = async (guid: string | null): Promise<DashboardLayout> => {
     const response = await dashboardApi.getDashboard(guid);
-    // if (response.status !== 200) return;
-    // else {
-    console.log(JSON.stringify(response));
     const layout = JSON.parse(response.data.data[0].layoutConfig);
     let panels = JSON.stringify(layout.panels);
     panels = panels.replace(/\"userWidgetIds\":/g, '"widgets":');
     panels = JSON.parse(panels);
     layout.panels = panels;
     return layout;
-    // }
 };
 
 function createTabbedFitFitLayout(): DashboardLayout {
