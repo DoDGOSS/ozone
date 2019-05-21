1a26628ef0c0e5ef5ed343b06bc3b225bf77d9d8
Omar A. Jandali (CTR) <1555356052@mil>
2019-05-21 11:51:38 -0700

    DG-849; added api and dto's for the HelpModel; created a new dialog without cancel button for help; adjusted css to set curor as hand when hovering files; setup help dialog and tree; Full tree treversal based on HelpFiles return object; Included a Read_me file for instructions on how to add help files

--- ozone-framework-client/packages/application/src/components/help-screen/HelpDialog.tsx
+++ ozone-framework-client/packages/application/src/components/help-screen/HelpDialog.tsx
@@ -1,17 +1,148 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import { useBehavior } from "../../hooks";
 
-import { Classes, Dialog } from "@blueprintjs/core";
-
+import { Dialog, ITreeNode, Spinner } from "@blueprintjs/core";
 import { mainStore } from "../../stores/MainStore";
-
 import { classNames } from "../../utility";
 
+import { showConfirmationDialogWithoutCancel } from "../confirmation-dialog/InPlaceConfirmationDialogWithoutCancel";
+
 import * as styles from "./index.scss";
+
+import { helpApi } from "../../api/clients/HelpAPI";
+import { HelpDTO } from "./../../api/models/HelpDTO";
+
+import { HelpTree } from "./HelpTree";
 
 export const HelpDialog: React.FC = () => {
     const themeClass = useBehavior(mainStore.themeClass);
     const isOpen = useBehavior(mainStore.isHelpDialogVisible);
+
+    const [files, setFiles] = useState([]);
+
+    const [folderLoading, setFolderLoading] = useState(false);
+
+    const InitialState: ITreeNode[] | undefined = [];
+
+    let currentNode: HelpDTO;
+    let parentFolder: any = {};
+    let majorFolder: any = {};
+    let majorFile: any = {};
+
+    useEffect(() => {
+        if (isOpen) {
+            fetchHelp();
+        }
+    }, [isOpen]);
+
+    const fetchHelp = () => {
+        helpApi.getHelpFiles().then((response) => {
+            if (response.status !== 200) return;
+            setFiles(response.data);
+            checkFileContent();
+        });
+    };
+
+    const checkFileContent = () => {
+        setTimeout(() => {
+            setFolderLoading(true);
+        }, 2000);
+    };
+
+    const closeDialogBox = () => {
+        mainStore.hideHelpDialog();
+        setFolderLoading(false);
+    };
+
+    const setNodes = (data: any) => {
+        if (data.length === 0) {
+            showConfirmationDialogWithoutCancel({
+                title: "Warning",
+                message: ["There are no help files in the Help Folder. Contact admin."],
+                onConfirm: () => {
+                    mainStore.hideHelpDialog();
+                }
+            });
+        } else {
+            for (const item of data) {
+                currentNode = item;
+                if (item["children"]) {
+                    createFolder(currentNode);
+                    traverse(currentNode);
+                    InitialState.push(majorFolder);
+                } else {
+                    createFile(currentNode);
+                    InitialState.push(majorFile);
+                }
+            }
+            return InitialState;
+        }
+    };
+
+    const traverse = (node: any) => {
+        checkChildFiles(node);
+        if (node["children"]) {
+            for (const item of node["children"]) {
+                if (item["children"]) {
+                    assignChildFolder(item);
+                    traverse(item);
+                }
+            }
+        }
+    };
+
+    const checkChildFiles = (node: any) => {
+        if(node['children']){
+            for (let file of node['children']) {
+                if (file["children"] === null || file["children"] === undefined) {
+                    assignChildFile(file);
+                }
+            }
+        }
+    };
+
+    const createFolder = (node: HelpDTO) => {
+        const folder = {
+            id: node.path,
+            label: node.text,
+            hasCaret: true,
+            icon: "folder-close",
+            childNodes: []
+        };
+        majorFolder = folder;
+        parentFolder = folder;
+    };
+
+    const createFile = (node: HelpDTO) => {
+        const file = {
+            id: node.path,
+            label: node.text,
+            icon: "document"
+        };
+        majorFile = file;
+    };
+
+    const assignChildFolder = (node: HelpDTO) => {
+        const folder = {
+            id: node.path,
+            label: node.text,
+            hasCaret: true,
+            icon: "folder-close",
+            childNodes: []
+        };
+        parentFolder["childNodes"].unshift(folder);
+        parentFolder = folder;
+    };
+
+    const assignChildFile = (node: HelpDTO) => {
+        const file = {
+            id: node.path,
+            label: node.text,
+            hasCaret: false,
+            icon: "document"
+        };
+        parentFolder["childNodes"].push(file);
+    };
 
     return (
         <Dialog
@@ -19,14 +150,11 @@ export const HelpDialog: React.FC = () => {
             title="Help"
             icon="help"
             isOpen={isOpen}
-            onClose={mainStore.hideHelpDialog}
+            onOpened={fetchHelp}
+            onClose={closeDialogBox}
         >
-            <div className={classNames(Classes.DIALOG_BODY, styles.helpContent)}>
-                <p>Lorem ipsum...</p>
-            </div>
-
-            <div className={Classes.DIALOG_FOOTER}>
-                <div className={Classes.DIALOG_FOOTER_ACTIONS} />
+            <div style={{ overflowY: "scroll" }}>
+                {folderLoading ? <HelpTree nodes={setNodes(files)} /> : <Spinner />}
             </div>
         </Dialog>
     );
