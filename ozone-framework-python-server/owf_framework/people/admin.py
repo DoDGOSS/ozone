6e031ccff01796d58a729b60f5c0ae411d5a4d05
Yancy Matherne <yancy.matherne@geocent.com>
2019-06-04 12:52:00 -0500

    DG-998: Add person to widget definition relationship.

--- ozone-framework-python-server/owf_framework/people/admin.py
+++ ozone-framework-python-server/owf_framework/people/admin.py
@@ -1,22 +1,26 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
+from owf_framework.owf_groups.admin import OWFGroupPeopleInline
 from .models import Person
 
-from owf_framework.owf_groups.admin import OWFGroupPeopleInline
+class PersonWidgetDefinitionInline(admin.TabularInline):
+    model = Person.widget_definitions.through
+
 
 class PersonAdmin(admin.ModelAdmin):
     list_display = ('id', 'user_real_name', 'username', 'email', 'description')
     search_fields = ('user_real_name', 'username', 'email')
-    inlines = [OWFGroupPeopleInline,]
+    inlines = [OWFGroupPeopleInline, PersonWidgetDefinitionInline,]
+
 
 admin.site.register(Person, PersonAdmin)
-
 
 class PersonInline(admin.StackedInline):
     model = Person
     verbose_name_plural = 'Person'
     fk_name = 'user'
+
 
 class CustomUserAdmin(UserAdmin):
     inlines = (PersonInline, )
--- ozone-framework-python-server/owf_framework/people/models.py
+++ ozone-framework-python-server/owf_framework/people/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User, AbstractUser
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from owf_framework.widgets.models import WidgetDefinition
 
 class Person(models.Model):
     id = models.BigAutoField(primary_key=True)
@@ -17,6 +18,7 @@ class Person(models.Model):
     email = models.CharField(max_length=255, blank=True, null=True)
     last_login = models.DateTimeField(blank=True, null=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    widget_definitions = models.ManyToManyField(WidgetDefinition, through='PersonWidgetDefinition')
 
     def __str__(self):
         return self.user.username
@@ -29,3 +31,20 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
         Person.objects.create(user=instance)
     instance.person.save()
+
+
+class PersonWidgetDefinition(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    version = models.BigIntegerField(default=1)
+    pwd_position = models.IntegerField(blank=False)
+    favorite = models.BooleanField(blank=True, null=True)
+    user_widget = models.BooleanField(blank=True, null=True)
+    disabled = models.BooleanField(blank=True, null=True)
+    widget_definition = models.ForeignKey(WidgetDefinition, on_delete=models.CASCADE)
+    display_name = models.CharField(null=True, blank=True, max_length=256)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    group_widget = models.BooleanField(blank=True, null=True)
+    visible = models.BooleanField(blank=False, null=False, default=True)
+
+    class Meta:
+        db_table = 'person_widget_definition'
