8a16ded67b771abc23cbfd3243edaeff87b798c8
Yancy Matherne <yancy.matherne@geocent.com>
2019-06-04 13:22:39 -0500

    DG-998: Add stack to owf group relationship.

--- ozone-framework-python-server/owf_framework/stacks/models.py
+++ ozone-framework-python-server/owf_framework/stacks/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from owf_framework.owf_groups.models import OWFGroup
 
 class Stack(models.Model):
     id = models.BigAutoField(primary_key=True)
@@ -12,9 +13,18 @@ class Stack(models.Model):
     image_url = models.CharField(null=True, blank=True, max_length=2083)
     descriptor_url = models.CharField(null=True, blank=True, max_length=2083)
     description = models.CharField(blank=True, null=True, max_length=2000)
+    groups = models.ManyToManyField(OWFGroup, through='StackGroup')
 
     def __str__(self):
         return self.name
 
     class Meta:
         db_table = 'stack'
+
+
+class StackGroup(models.Model):
+    group = models.ForeignKey(OWFGroup, on_delete=models.CASCADE)
+    stack = models.ForeignKey(Stack, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'stack_groups'
