6ec342b982e52bb0e70d40e58adfb24072966416
Yancy Matherne <yancy.matherne@geocent.com>
2019-06-04 02:10:40 -0500

    DG-998: Add stack domain model.

---
+++ ozone-framework-python-server/owf_framework/stacks/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Stack(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    version = models.BigIntegerField(default=1)
+    owner_id = models.BigIntegerField(blank=True, null=True)
+    stack_context = models.CharField(null=False, blank=False, max_length=200)
+    default_group_id = models.BigIntegerField(blank=True, null=True)
+    unique_widget_count = models.IntegerField(blank=False)
+    name = models.CharField(null=False, blank=False, max_length=256)
+    approved = models.BooleanField(blank=True, null=True)
+    image_url = models.CharField(null=True, blank=True, max_length=2083)
+    descriptor_url = models.CharField(null=True, blank=True, max_length=2083)
+    description = models.CharField(blank=True, null=True, max_length=2000)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'stack'
