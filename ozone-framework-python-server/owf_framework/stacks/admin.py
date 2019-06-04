dfe08d1659ac15d145f46af4734fa8aab0d2edea
Yancy Matherne <yancy.matherne@geocent.com>
2019-06-04 13:22:39 -0500

    DG-998: Add stack to owf group relationship.

--- ozone-framework-python-server/owf_framework/stacks/admin.py
+++ ozone-framework-python-server/owf_framework/stacks/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import Stack
 
+class StackGroupInline(admin.TabularInline):
+    model = Stack.groups.through
+
 class StackAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'description')
     search_fields = ('name',)
+    inlines = [StackGroupInline,]
 
 admin.site.register(Stack, StackAdmin)
