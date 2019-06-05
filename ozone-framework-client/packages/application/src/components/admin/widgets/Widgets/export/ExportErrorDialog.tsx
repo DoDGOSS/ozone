a2f74de2c2e18441e3c48752eab15db4bc70c427
Yancy Matherne <yancy.matherne@geocent.com>
2019-06-04 20:58:50 -0500

    DG-777: Fix linter errors for export widget.

--- ozone-framework-client/packages/application/src/components/admin/widgets/Widgets/export/ExportErrorDialog.tsx
+++ ozone-framework-client/packages/application/src/components/admin/widgets/Widgets/export/ExportErrorDialog.tsx
@@ -4,36 +4,36 @@ import * as React from "react";
 import { WidgetDTO } from "../../../../../api/models/WidgetDTO";
 
 export interface ExportErrorDialogProps {
-  widget: WidgetDTO;
-  onClose: () => void;
+    widget: WidgetDTO;
+    onClose: () => void;
 }
 
 export const ExportErrorDialog: React.FC<ExportErrorDialogProps> = (props) => {
-  return (<div>
-    <Dialog
-        isOpen={true}
-        isCloseButtonShown={false}
-        onClose={props.onClose}
-        canOutsideClickClose={false}
-        title="Server Error!"
-        data-element-id="export-error-dialog"
-    >
-        <div className={Classes.DIALOG_BODY}>
-            The export of widget {props.widget.value.namespace} failed.
-        </div>
+    return (
+        <div>
+            <Dialog
+                isOpen={true}
+                isCloseButtonShown={false}
+                onClose={props.onClose}
+                canOutsideClickClose={false}
+                title="Server Error!"
+                data-element-id="export-error-dialog"
+            >
+                <div className={Classes.DIALOG_BODY}>The export of widget {props.widget.value.namespace} failed.</div>
 
-        <div className={Classes.DIALOG_FOOTER}>
-            <div className={Classes.DIALOG_FOOTER_ACTIONS}>
-                <Button
-                    onClick={props.onClose}
-                    intent={Intent.SUCCESS}
-                    rightIcon="tick"
-                    data-element-id="export-error-dialog-confirm"
-                >
-                    OK
-                </Button>
-            </div>
+                <div className={Classes.DIALOG_FOOTER}>
+                    <div className={Classes.DIALOG_FOOTER_ACTIONS}>
+                        <Button
+                            onClick={props.onClose}
+                            intent={Intent.SUCCESS}
+                            rightIcon="tick"
+                            data-element-id="export-error-dialog-confirm"
+                        >
+                            OK
+                        </Button>
+                    </div>
+                </div>
+            </Dialog>
         </div>
-    </Dialog>
-  </div>);
+    );
 };
