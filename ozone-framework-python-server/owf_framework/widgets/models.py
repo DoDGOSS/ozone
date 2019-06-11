608a20e69535ff2f92ced99edf345792a7fa2b24
Yancy Matherne <yancy.matherne@geocent.com>
2019-06-11 00:39:05 -0500

    DG-998: Add model constraints

--- ozone-framework-python-server/owf_framework/widgets/models.py
+++ ozone-framework-python-server/owf_framework/widgets/models.py
@@ -1,18 +1,19 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 
 class WidgetDefinition(models.Model):
     id = models.BigAutoField(primary_key=True)
     version = models.BigIntegerField(default=1)
-    width = models.IntegerField(blank=False)
+    width = models.IntegerField(default=0, validators=[MinValueValidator(0)], blank=False)
     image_url_medium = models.CharField(null=False, blank=False, max_length=2083)
     singleton = models.BooleanField(blank=False, default=False)
     universal_name = models.CharField(null=True, blank=True, max_length=255)
     display_name = models.CharField(null=False, blank=False, max_length=256)
-    widget_guid = models.CharField(null=False, blank=False, max_length=255)
+    widget_guid = models.CharField(unique=True, null=False, blank=False, max_length=255)
     mobile_ready = models.BooleanField(blank=False, default=False)
     widget_version = models.CharField(null=True, blank=True, max_length=2083)
-    height = models.IntegerField(blank=False)
+    height = models.IntegerField(default=0, validators=[MinValueValidator(0)], blank=False)
     background = models.BooleanField(blank=False, default=False)
     widget_url = models.CharField(null=False, blank=False, max_length=2083)
     image_url_small = models.CharField(null=False, blank=False, max_length=2083)
