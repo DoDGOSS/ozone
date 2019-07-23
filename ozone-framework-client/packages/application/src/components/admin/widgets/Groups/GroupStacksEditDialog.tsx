ee49724435f6a436a7725c7e9a6dbd75e9952437
William Ehrlich <william.d.ehrlich2.ctr@mail.mil>
2019-07-23 09:49:47 -0700

    Only shared stacks can be added to a User/Group via Admin Widget

--- ozone-framework-client/packages/application/src/components/admin/widgets/Groups/GroupStacksEditDialog.tsx
+++ ozone-framework-client/packages/application/src/components/admin/widgets/Groups/GroupStacksEditDialog.tsx
@@ -36,6 +36,6 @@ export class GroupStacksEditDialog extends React.Component<SelectionDialogProps<
 
         if (response.status !== 200) return [];
 
-        return response.data.data;
+        return response.data.data.filter((stack) => stack.approved);
     }
 }
