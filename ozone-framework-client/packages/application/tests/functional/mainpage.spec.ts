14d5206a996c4eb559b1daf7b5227d0d0fe8cd62
Leo Reyes <leo.reyes@geocent.com>
2019-07-24 16:56:30 -0500

    Added loading of previously selected dashboard.
    * sets the dashboard as the default when setting a new current dashboard and removes the previous dashboard as the default.

--- ozone-framework-client/packages/application/tests/functional/mainpage.spec.ts
+++ ozone-framework-client/packages/application/tests/functional/mainpage.spec.ts
@@ -1,6 +1,6 @@
 import { NightwatchAPI } from "nightwatch";
 
-import { MainPage } from "./selectors";
+import { MainPage, StackDialog } from "./selectors";
 
 import { loggedInAs } from "./helpers";
 
@@ -41,5 +41,36 @@ module.exports = {
         );
 
         browser.closeWindow().end();
+    },
+    "As a User, I should see my last selected dashboard as the initial dashboard that loads": (
+        browser: NightwatchAPI
+    ) => {
+        loggedInAs(browser, "testUser1", "password", "Test User 1");
+
+        //TODO: create a way to select a stack/dashboard from the list. The tree will need to be updated to include some attribute for the stack/dashboard that we can query against.
+        browser.waitForElementVisible(MainPage.STACKS_BUTTON, 2000, "[Stacks Button] is visible.");
+        browser
+            .click(MainPage.STACKS_BUTTON)
+            .waitForElementVisible(StackDialog.CREATE_STACK_BUTTON, 2000, "[Create Stack Button] is visible.");
+        browser
+            .click(StackDialog.CREATE_STACK_BUTTON)
+            .waitForElementVisible(StackDialog.CreateStack.SUBMIT, 2000, "[Create Stack Submit Button] is visible.");
+
+        browser.setValue(StackDialog.CreateStack.NAME_FIELD, StackDialog.CreateStack.CREATE_STACK_NAME);
+
+        browser
+            .click(StackDialog.CreateStack.SUBMIT)
+            .waitForElementNotPresent(StackDialog.CreateStack.SUBMIT, 1000, "[Create Stack Dialog] is closed.");
+
+        browser.refresh();
+        browser.waitForElementVisible(MainPage.STACKS_BUTTON, 2000, "[Stacks Button] is visible.");
+        browser.getTitle(function(title) {
+            if (title)
+                this.assert.equal(
+                    title,
+                    `Ozone - ${StackDialog.CreateStack.CREATE_STACK_NAME} (default)`,
+                    "[Dashboard] is last selected dashboard."
+                );
+        });
     }
 };
