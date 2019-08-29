9142fb67e8abc96a2777a7166556d9757b92fe2c
Ryan Carl <ryan.carl@geocent.com>
2019-08-29 13:35:38 -0500

    widgets admin alter perm and url

--- ozone-framework-python-server/widgets/views.py
+++ ozone-framework-python-server/widgets/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-# from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAdminUser
 from .models import WidgetDefinition
 from .serializers import WidgetDefinitionSerializer
 
@@ -10,6 +10,8 @@ class WidgetDefinitionViewSet(viewsets.ModelViewSet):
     """
     queryset = WidgetDefinition.objects.all()
     serializer_class = WidgetDefinitionSerializer
+    permission_classes = (IsAdminUser,)
+
 
 
 
