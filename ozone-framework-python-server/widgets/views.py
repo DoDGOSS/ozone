9e22a828c375fb2da810d9ee777b831dd6cf8347
Ryan Carl <ryan.carl@geocent.com>
2019-08-30 12:50:54 -0500

    feature/DG-1127 widget types url and test for getting widget types + expantion

--- ozone-framework-python-server/widgets/views.py
+++ ozone-framework-python-server/widgets/views.py
@@ -1,15 +1,24 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAdminUser
-from .models import WidgetDefinition
-from .serializers import WidgetDefinitionSerializer
+from .models import WidgetDefinition, WidgetType
+from .serializers import WidgetDefinitionSerializer, WidgetTypeSerializer
 
 
 class WidgetDefinitionViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows people to be viewed or edited.
+    API endpoint that allows Widget to be viewed or edited.
     """
     queryset = WidgetDefinition.objects.all()
     serializer_class = WidgetDefinitionSerializer
+    permission_classes = (IsAdminUser,)
+
+
+class WidgetTypesViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows widget types to be viewed or edited.
+    """
+    queryset = WidgetType.objects.all()
+    serializer_class = WidgetTypeSerializer
     permission_classes = (IsAdminUser,)
 
 
