4d3cc0d7d604ea5856b39f7f4c58a26a09861ebf
Ryan Carl <ryan.carl@geocent.com>
2019-09-13 16:01:59 -0500

    Feature/dg 1250 merg conflicts

--- ozone-framework-python-server/people/administration/serializers.py
+++ ozone-framework-python-server/people/administration/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from people.models import Person
+from owf_groups.models import OwfGroupPeople, OwfGroup
+from django.conf import settings
 
 
 class AdministrationOfUserSerializer(serializers.ModelSerializer):
@@ -7,16 +9,36 @@ class AdministrationOfUserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Person
-        fields = '__all__'
+        fields = ('email', 'username', 'password', 'is_admin')
 
     def create(self, validated_data):
-        user = super(AdministrationOfUserSerializer, self).create(validated_data)
-        user.set_password(validated_data['password'])
-        user.save()
+        if validated_data['is_admin']:
+            user = Person.objects.create_superuser(email=validated_data['email'], username=validated_data['username'],
+                                                   password=validated_data['password'])
+            return user
+        else:
+            user = Person.objects.create_user(email=validated_data['email'], username=validated_data['username'],
+                                              password=validated_data['password'])
         return user
 
+
+class AdministrationOfUserSerializerFull(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = Person
+        fields = '__all__'
+
     def update(self, instance, validated_data):
+        if not validated_data['is_admin'] and instance.is_admin is True:
+            group = OwfGroup.objects.get(name=settings.DEFAULT_ADMIN_GROUP)
+            remove_from = OwfGroupPeople.objects.get(group_id=group.id, person_id=instance.id)
+            remove_from.delete()
+        if validated_data['is_admin'] and instance.is_admin is False:
+            group = OwfGroup.objects.get(name=settings.DEFAULT_ADMIN_GROUP)
+            add_to_group = OwfGroupPeople(group_id=group.id, person_id=instance.id)
+            add_to_group.save()
         if 'password' in validated_data:
             password = validated_data.pop('password')
             instance.set_password(password)
-        return super(AdministrationOfUserSerializer, self).update(instance, validated_data)
+        return super(AdministrationOfUserSerializerFull, self).update(instance, validated_data)
--- ozone-framework-python-server/people/administration/views.py
+++ ozone-framework-python-server/people/administration/views.py
@@ -1,10 +1,18 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.permissions import IsAdminUser
 from people.models import Person
-from .serializers import AdministrationOfUserSerializer
+from .serializers import AdministrationOfUserSerializer, AdministrationOfUserSerializerFull
 
 
 class AdministrationOfUserAPIView(viewsets.ModelViewSet):
     queryset = Person.objects.all()
-    serializer_class = AdministrationOfUserSerializer
     permission_classes = (IsAdminUser,)
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['username']
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return AdministrationOfUserSerializer
+        else:
+            return AdministrationOfUserSerializerFull
