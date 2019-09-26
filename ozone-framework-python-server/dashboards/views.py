e1f2908561998e6f6b473f01e2869df2ccea3179
Leo Reyes <leo.reyes@geocent.com>
2019-09-25 22:35:15 -0500

    * added deleting of a dashboard. * fixed issues with removing user from group

--- ozone-framework-python-server/dashboards/views.py
+++ ozone-framework-python-server/dashboards/views.py
@@ -4,14 +4,24 @@ from .permissions import IsStackOwner
 from .models import Dashboard
 from .serializers import DashboardBaseSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class DashboardViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows dashboards to be viewed or edited.
     """
+    # TODO: filter out dashboard's that are marked for deletion on a GET
     queryset = Dashboard.objects.all()
     serializer_class = DashboardBaseSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = (IsAuthenticated, IsStackOwner)
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['guid', ]
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if(instance.user == request.user):
+            instance.marked_for_deletion = True
+            instance.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
