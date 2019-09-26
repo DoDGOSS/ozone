9fd6a3b63478b047b62da234d9d1376e6a0e6f90
Leo Reyes <leo.reyes@geocent.com>
2019-09-25 22:35:15 -0500

    * added deleting of a dashboard. * fixed issues with removing user from group

--- ozone-framework-python-server/dashboards/permissions.py
+++ ozone-framework-python-server/dashboards/permissions.py
@@ -4,9 +4,12 @@ from stacks.models import Stack
 
 class IsStackOwner(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
+        if request.method != "POST":
             return True
 
         stack_id = request.data['stack']
         stack = Stack.objects.get(pk=stack_id)
         return stack.owner == request.user
+
+    def has_object_permission(self, request, view, obj):
+        return obj.stack.owner == request.user
