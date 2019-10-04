5219e85fe1e10d443419d7f9dc2e98820a5eb7dc
Ryan Carl <ryan.carl@geocent.com>
2019-10-04 12:53:30 -0500

    DG-1043 As a admin, I want to update configuration settings

--- ozone-framework-python-server/tests/appconf/test_appconf.py
+++ ozone-framework-python-server/tests/appconf/test_appconf.py
@@ -1,14 +1,12 @@
 from django.urls import reverse
 from rest_framework.test import APIClient
 from django.test import TestCase
+from people.models import Person
 
 requests = APIClient()
 
-payload = {
-            "version": 1,
-            "created_date": "2019-08-29",
-            "edited_date": "2019-08-29",
-            "code": "owf.enable.cef.logging",
+post_payload = {
+            "code": "test_payload_owf.enable.cef.logging",
             "value": "False",
             "title": "Enable CEF Logging Update",
             "description": "None",
@@ -18,36 +16,90 @@ payload = {
             "mutable": False,
             "sub_group_order": 1,
             "help": "help 2",
-            "created_by": 1,
-            "edited_by": 1
+}
+
+patch_payload = {"description": "Has Changed"}
+
+put_payload = {
+            "code": "test_payload_owf.enable.cef.logging",
+            "value": "False",
+            "title": "Enable CEF Logging Update",
+            "description": "Change PUT",
+            "type": "Boolean",
+            "group_name": "AUDITING",
+            "sub_group_name": "None",
+            "mutable": False,
+            "sub_group_order": 1,
+            "help": "help 2",
 }
 
 
-class TestingPersonBaseUrl(TestCase):
+class TestingApplicationConfigAPI(TestCase):
     fixtures = ['people_data.json', 'appconf_data.json']
 
-    def test_access_users_url(self):
+    def test_admin_can_create_appconf_data(self):
+        admin_user = Person.objects.get(email='admin@goss.com')
+
+        requests.login(email='admin@goss.com', password='password')
+        url = reverse('applicationconfiguration-list')
+        response = requests.post(url, post_payload)
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.data['edited_by'], None)
+        self.assertEqual(response.data['created_by'], admin_user.id)
+
+        requests.logout()
+
+    def test_admin_can_list_appconf_endpoint(self):
+
         requests.login(email='admin@goss.com', password='password')
         url = reverse('applicationconfiguration-list')
         data = requests.get(url)
+
         self.assertEqual(data.status_code, 200)
+
         requests.logout()
+
+    def test_admin_can_patch_appconf_data(self):
+        admin_user = Person.objects.get(email='admin@goss.com')
+
+        requests.login(email='admin@goss.com', password='password')
+        url = reverse('applicationconfiguration-detail', args='1')
+        response = requests.patch(url, patch_payload)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['edited_by'], admin_user.id)
+
+        requests.logout()
+
+    def test_admin_can_put_appconf_data(self):
+        admin_user = Person.objects.get(email='admin@goss.com')
+
+        requests.login(email='admin@goss.com', password='password')
+        url = reverse('applicationconfiguration-detail', args='1')
+        response = requests.put(url, put_payload)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['edited_by'], admin_user.id)
+
+        requests.logout()
+
+    def test_admin_can_delete_appconf_data(self):
+
+        requests.login(email='admin@goss.com', password='password')
+        url = reverse('applicationconfiguration-detail', args='1')
+        response = requests.delete(url)
+
+        self.assertEqual(response.status_code, 204)
+
+        requests.logout()
+
+    def test_admin_auth_only_appconf(self):
+
         requests.login(email='user@goss.com', password='password')
         url = reverse('applicationconfiguration-list')
         data = requests.get(url)
-        self.assertEqual(data.status_code, 403)
-        requests.logout()
 
-    def test_appconf_update(self):
-        # URL
-        url = reverse('applicationconfiguration-detail', args='1')
-        # Login
-        requests.login(email='admin@goss.com', password='password')
-        # Request preload
-        getting = requests.get(url)
-        # Request Change
-        response = requests.patch(url, data=payload)
-        # Tests status and data validation
-        self.assertEqual(response.status_code, 200)
-        self.assertNotEquals(getting.data['title'], response.data['title'])
-        self.assertEqual(response.data['title'], "Enable CEF Logging Update")
+        self.assertEqual(data.status_code, 403)
+
+        requests.logout()
