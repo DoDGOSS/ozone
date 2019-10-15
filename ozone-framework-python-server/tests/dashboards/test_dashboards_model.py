a082a3eaa6950ff5f284393ec63bdb938dd85add
Leo Reyes <leo.reyes@geocent.com>
2019-10-15 11:08:04 -0500

    Restoring of dashboards

---
+++ ozone-framework-python-server/tests/dashboards/test_dashboards_model.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+from dashboards.models import Dashboard
+
+
+class DashboardModelTests(TestCase):
+    fixtures = [
+        'people_data.json',
+        'tests/dashboards/fixtures/default_test_dashboards.json'
+    ]
+
+    def test_restore_dashboard(self):
+        default_dashboard = Dashboard.objects.get(pk=1)
+        dashboard_to_restore = Dashboard.objects.get(pk=2)
+        dashboard_to_restore.restore()
+        dashboard_to_restore.refresh_from_db()
+
+        self.assertEqual(default_dashboard.name, dashboard_to_restore.name)
+        self.assertEqual(default_dashboard.description, dashboard_to_restore.description)
+        self.assertEqual(default_dashboard.icon_image_url, dashboard_to_restore.icon_image_url)
+        self.assertEqual(default_dashboard.type, dashboard_to_restore.type)
+        self.assertEqual(default_dashboard.locked, dashboard_to_restore.locked)
+        self.assertEqual(default_dashboard.marked_for_deletion, dashboard_to_restore.marked_for_deletion)
+        self.assertEqual(default_dashboard.layout_config, dashboard_to_restore.layout_config)
