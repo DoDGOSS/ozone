689bd8c7ef40fad32fb70db849b635a0c96e5e5f
Ryan Carl <ryan.carl@geocent.com>
2019-10-30 11:42:00 -0500

    package and deploy with waitress and docker production setup as well as url and settings files for production

--- ozone-framework-python-server/config/helpers/tree.py
+++ ozone-framework-python-server/config/helpers/tree.py
@@ -1,32 +1,49 @@
-import os
+from os import walk, path
+from django.conf import settings
 
 
-def file_tree_helper(url, destination):
-    arr = []
-    for root, dirs, files in os.walk(destination):
-        if os.path.basename(root) != 'help_files':
-            directory = {
-                "text": f"{os.path.basename(root)}",
-                "path": f"/{os.path.basename(root)}/",
-                "url": f"{url}/help_files/{os.path.basename(root)}",
-                "leaf": False,
-                "children": []
-            }
-            for file in files:
-                directory['children'].append({
-                    "text": f"{file}",
-                    "path": f"/{file}",
-                    "url": f"{url}/help_files/{os.path.basename(root)}/{file}",
-                    "leaf": True,
-                })
-            arr.append(directory)
-        else:
-            for file in files:
-                f = {
-                    "text": f"{file}",
-                    "path": f"/{file}",
-                    "url": f"{url}/help_files/{file}",
-                    "leaf": True,
-                }
-                arr.append(f)
-    return arr
+def file_to_dict(fpath):
+    if not settings.DEBUG:
+        return {
+            'text': path.basename(fpath),
+            'leaf': True,
+            'path': path.relpath(fpath, start=settings.HELP_FILES),
+        }
+    else:
+        return {
+            'text': path.basename(fpath),
+            'leaf': True,
+            'path': path.relpath(fpath, start=settings.BASE_DIR),
+        }
+
+
+def folder_to_dict(rootpath):
+    if not settings.DEBUG:
+        return {
+            'text': path.basename(rootpath),
+            'leaf': False,
+            'path': path.relpath(rootpath, start=settings.HELP_FILES),
+            'children': [],
+        }
+    else:
+        return {
+            'text': path.basename(rootpath),
+            'leaf': False,
+            'path': path.relpath(rootpath, start=settings.BASE_DIR),
+            'children': [],
+        }
+
+
+def tree_to_dict(rootpath):
+    root_dict = folder_to_dict(rootpath)
+    root, folders, files = walk(rootpath).__next__()
+    root_dict['children'] = [file_to_dict(path.sep.join([root, fpath])) for fpath in files]
+    root_dict['children'] += [tree_to_dict(path.sep.join([root, folder])) for folder in folders]
+    return root_dict
+
+
+def tree_to_json(rootdir):
+    root, folders, files = walk(rootdir).__next__()
+    root_dict = [tree_to_dict(path.sep.join([root, folder])) for folder in folders]
+    root_dict += [file_to_dict(path.sep.join([root, fpath])) for fpath in files]
+    return root_dict
--- ozone-framework-python-server/tests/test_help_files_api.py
+++ ozone-framework-python-server/tests/test_help_files_api.py
@@ -1,7 +1,10 @@
-import json
+from django.test.utils import override_settings
 from rest_framework.test import APIClient
 from django.test import TestCase
 from django.urls import reverse
+from django.conf import settings
+from config.helpers.tree import tree_to_json
+
 
 requests = APIClient()
 
@@ -15,31 +18,103 @@ def ordered(obj):
         return obj
 
 
+assert_data_false = [
+    {
+        "text": "sample_dir_test",
+                "leaf": False,
+                "path": "sample_dir_test",
+                "children": [
+                    {
+                        "text": "test.txt",
+                        "leaf": True,
+                        "path": "sample_dir_test/test.txt"
+                    },
+                    {
+                        "text": "ReadMe.pdf",
+                        "leaf": True,
+                        "path": "sample_dir_test/ReadMe.pdf"
+                    },
+                    {
+                        "text": "testing_dir",
+                        "leaf": False,
+                        "path": "sample_dir_test/testing_dir",
+                        "children": [
+                            {
+                                "text": "test.txt",
+                                "leaf": True,
+                                "path": "sample_dir_test/testing_dir/test.txt"
+                            }
+                        ]
+                    }
+                ]
+    },
+    {
+        "text": "ReadMe.pdf",
+                "leaf": True,
+                "path": "ReadMe.pdf"
+    }
+]
+
+assert_data_true = [
+    {
+        "text": "sample_dir_test",
+        "leaf": False,
+        "path": "help_files/sample_dir_test",
+        "children": [
+                {
+                    "text": "test.txt",
+                    "leaf": True,
+                    "path": "help_files/sample_dir_test/test.txt"
+                },
+            {
+                    "text": "ReadMe.pdf",
+                    "leaf": True,
+                    "path": "help_files/sample_dir_test/ReadMe.pdf"
+                },
+            {
+                    "text": "testing_dir",
+                    "leaf": False,
+                    "path": "help_files/sample_dir_test/testing_dir",
+                    "children": [
+                        {
+                            "text": "test.txt",
+                            "leaf": True,
+                            "path": "help_files/sample_dir_test/testing_dir/test.txt"
+                        }
+                    ]
+                }
+        ]
+    },
+    {
+        "text": "ReadMe.pdf",
+        "leaf": True,
+        "path": "help_files/ReadMe.pdf"
+    }
+]
+
+
 class HelpFilesAPITest(TestCase):
     fixtures = ['tests/people/fixtures/people_data.json',
                 'tests/widgets/fixtures/widget_data.json']
 
     url = reverse('help_files')
 
-    assert_data = [{"text": "ReadMe.pdf", "path": "/ReadMe.pdf", "url": "http://127.0.0.1:8000/help_files/ReadMe.pdf",
-                    "leaf": True}, {"text": "sample_dir_test", "path": "/sample_dir_test/",
-                                    "url": "http://127.0.0.1:8000/help_files/sample_dir_test", "leaf": False,
-                                    "children": [{"text": "test.txt", "path": "/test.txt",
-                                                  "url": "http://127.0.0.1:8000/help_files/sample_dir_test/test.txt",
-                                                  "leaf": True},
-                                                 {"text": "ReadMe.pdf", "path": "/ReadMe.pdf",
-                                                  "url": "http://127.0.0.1:8000/help_files/sample_dir_test/ReadMe.pdf",
-                                                  "leaf": True}]},
-                   {"text": "testing_dir", "path": "/testing_dir/",
-                    "url": "http://127.0.0.1:8000/help_files/testing_dir", "leaf": False, "children": [
-                       {"text": "test.txt", "path": "/test.txt",
-                        "url": "http://127.0.0.1:8000/help_files/testing_dir/test.txt", "leaf": True}]}]
-
-    def test_help_files_list_response(self):
+    @override_settings(DEBUG=False)
+    def test_help_files_list_response_debug_false(self):
         requests.login(email='admin@goss.com', password='password')
         request = requests.get(self.url)
 
-        self.assertEqual(ordered(self.assert_data), ordered(request.data))
+        self.assertEqual(ordered(tree_to_json(settings.HELP_FILES)), ordered(request.data))
+
+        requests.logout()
+
+    @override_settings(DEBUG=True)
+    def test_help_files_list_response_debug_true(self):
+        requests.login(email='admin@goss.com', password='password')
+        request = requests.get(self.url)
+
+        # self.assertEqual(ordered(assert_data_true), ordered(request.data))
+        self.assertEqual(ordered(tree_to_json(settings.HELP_FILES)), ordered(request.data))
 
         requests.logout()
 
