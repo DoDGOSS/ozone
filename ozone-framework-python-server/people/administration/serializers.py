77225d32cb0b39ab059c47dd9eb1e22fe8329dd4
Ahsan Masood <ahsan.masood@geocent.com>
2019-11-21 00:33:11 -0600

    fixed user creation without password and made changes which would allow that

--- ozone-framework-python-server/people/administration/serializers.py
+++ ozone-framework-python-server/people/administration/serializers.py
@@ -7,20 +7,20 @@ from django.conf import settings
 
 
 class AdministrationOfUserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True, required=False)
 
     class Meta:
         model = Person
         fields = ('email', 'username', 'password', 'is_admin')
 
     def create(self, validated_data):
-        if validated_data['is_admin']:
+        if 'is_admin' in validated_data and validated_data['is_admin']:
             user = Person.objects.create_superuser(email=validated_data['email'], username=validated_data['username'],
-                                                   password=validated_data['password'])
+                                                   password=validated_data.get('password'))
             return user
         else:
             user = Person.objects.create_user(email=validated_data['email'], username=validated_data['username'],
-                                              password=validated_data['password'])
+                                              password=validated_data.get('password'))
         return user
 
 
