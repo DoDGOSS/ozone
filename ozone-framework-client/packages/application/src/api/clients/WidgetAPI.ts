5b3632dcd46c583e71f629a666f97b3d256dd25b
William Ehrlich <william.d.ehrlich2.ctr@mail.mil>
2019-11-26 01:00:47 -0800

    Fix Widget API delete call

--- ozone-framework-client/packages/application/src/api/clients/WidgetAPI.ts
+++ ozone-framework-client/packages/application/src/api/clients/WidgetAPI.ts
@@ -62,7 +62,7 @@ export class WidgetAPI {
     }
 
     async deleteWidget(id: number): Promise<Response<void>> {
-        return this.gateway.post(`admin/widgets/${id}/`, null, {
+        return this.gateway.delete(`admin/widgets/${id}/`, null, {
             headers: {
                 "Content-Type": "application/x-www-form-urlencoded"
             }
