c37b96a5977f83e2c9192798b75d23ed3e10fff8
Ahsan Masood <ahsan.masood@geocent.com>
2019-12-02 21:07:23 -0600

    Revert "docker white space issue for env and subsequent serializer"
    This reverts commit 47ec93e3199e173c86ff821414a4c769d1cc9541.

--- ozone-framework-python-server/legacy/views.py
+++ ozone-framework-python-server/legacy/views.py
@@ -6,7 +6,6 @@ from rest_framework.decorators import api_view, parser_classes, permission_class
 from rest_framework.parsers import FormParser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.schemas import AutoSchema, ManualSchema
 from rest_framework.viewsets import GenericViewSet
 
 from .serializers import PreferenceValueSerializer
@@ -15,19 +14,19 @@ from domain_mappings.models import RelationshipType, MappingType, DomainMapping
 
 from owf_groups.models import OwfGroup
 
-from people.models import PersonWidgetDefinition
 from people.serializers import PersonBaseSerializer
 
 from preferences.models import Preference
 from preferences.serializer import PreferenceSerializer
 
-from widgets.models import WidgetDefinition, WidgetType
-from widgets.serializers import WidgetDefinitionSerializer, WidgetTypeSerializer
+from widgets.models import WidgetDefinition
+from widgets.serializers import WidgetDefinitionSerializer
 
 from rest_framework.authentication import SessionAuthentication
 
 
-# This is currently a work around for the legacy endpoints, used by the client widget api that do not attach a csrf token to the requests.
+# This is currently a work around for the legacy endpoints,
+# used by the client widget api that do not attach a csrf token to the requests.
 class CsrfExemptSessionAuthentication(SessionAuthentication):
 
     def enforce_csrf(self, request):
@@ -53,14 +52,12 @@ class PreferencesViewSet(GenericViewSet):
             defaults={"value": value}
         )
         serialized = PreferenceSerializer(obj).data
-        del (serialized['version'])
         return Response(serialized)
 
     def get(self, request, namespace=None, path=None):
         try:
             pref = Preference.objects.get(user=request.user, namespace=namespace, path=path)
             serialized = PreferenceSerializer(pref).data
-            del (serialized['version'])
             return Response(serialized)
         except Preference.DoesNotExist:
             return Response({'success': True, 'preference': None})
@@ -77,11 +74,14 @@ def has_preference(request, namespace=None, path=None):
 
 @api_view(['GET'])
 @parser_classes([FormParser])
+@permission_classes([IsAuthenticated])
 def prefs_widget_list_user_and_group(request):
     filtered_ids = []
-    if 'group_id' in request.data:
+    lookup = {}
+
+    if 'group_id' in request.GET:
         try:
-            group = OwfGroup.objects.get(id=request.data['group_id'])
+            group = OwfGroup.objects.get(id=request.GET['group_id'])
             filtered_ids.extend(list(
                 DomainMapping.objects.filter(
                     src_id=group.id,
@@ -93,32 +93,31 @@ def prefs_widget_list_user_and_group(request):
         except OwfGroup.DoesNotExist:
             pass
 
-        if len(filtered_ids) == 0:
+        if len(filtered_ids) <= 0:
             return Response([])
+        else:
+            lookup = {'id__in': filtered_ids}
 
     # Setup filtering.
     queryset = WidgetDefinition.objects.filter()
-    if 'widgetName' in request.data:
-        widget_name = request.data['widgetName']
-        if widget_name.startswith('%') and widget_name.endswith('%'):
-            queryset = queryset.filter(display_name__contains=widget_name.strip('%'))
-        else:
-            queryset = queryset.filter(display_name=widget_name)
-    if 'widgetVersion' in request.data:
-        queryset = queryset.filter(widget_version=request.data['widgetVersion'])
-    if 'widgetGuid' in request.data:
-        queryset = queryset.filter(widget_guid=request.data['widgetGuid'])
-    if 'universalName' in request.data:
-        queryset = queryset.filter(universal_name=request.data['universalName'])
-    if len(filtered_ids) > 0:
-        queryset = queryset.filter(id__in=filtered_ids)
+    allowed_keys = {
+        'widget_name': 'display_name',
+        'widget_version': 'widget_version',
+        'widget_guid': 'widget_guid',
+        'universal_name': 'universal_name',
+    }
+    for k, v in request.GET.items():
+        if k in allowed_keys:
+            if v.startswith('%') or k.endswith('%'):
+                lookup = {f'{allowed_keys[k]}__contains': v.strip('%')}
+            else:
+                lookup = {allowed_keys[k]: v.strip('%')}
 
-    widgets = []
-    for widget in queryset.iterator():
-        model = _create_widget_definition_service_model(widget)
-        widgets.append(model)
+    if lookup:
+        queryset = queryset.filter(**lookup)
 
-    return Response(widgets)
+    serializer = WidgetDefinitionSerializer(queryset, many=True)
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
@@ -140,68 +139,3 @@ def whoami(request):
     converted['email'] = serialized_user['email']
 
     return Response(converted)
-
-
-def _create_widget_definition_service_model(widget):
-    serialized = WidgetDefinitionSerializer(widget).data
-
-    widget_guid = serialized['widget_guid']
-
-    total_users = PersonWidgetDefinition.objects.filter(widget_definition=widget).count()
-    total_groups = DomainMapping.objects.filter(
-        src_id=widget.id,
-        src_type=MappingType.widget,
-        relationship_type=RelationshipType.owns,
-        dest_type=MappingType.group,
-    ).count()
-
-    widget_types = []
-    for widget_type in widget.types.iterator():
-        widget_types.append(_create_widget_type_service_model(widget_type))
-
-    # Add fields.
-    serialized.update({
-        'totalUsers': total_users,
-        'totalGroups': total_groups,
-        'x': 0,
-        'y': 0,
-        'namespace': serialized['display_name'],
-        'url': serialized['widget_url'],
-        'headerIcon': serialized['image_url_medium'],
-        'image': serialized['image_url_small'],
-        'smallIconUrl': serialized['image_url_small'],
-        'mediumIconUrl': serialized['image_url_medium'],
-        'minimized': False,
-        'maximized': False,
-        'definitionVisible': serialized['visible'],
-        'widgetTypes': widget_types,
-    })
-
-    # Delete unneeded fields.
-    to_delete = [
-        'id',
-        'version',
-        'image_url_small',
-        'image_url_medium',
-        'widget_url',
-        'widget_guid',
-        'display_name',
-        'types',
-    ]
-    for field in to_delete:
-        del serialized[field]
-
-    model = {
-        'id': widget_guid,
-        'namespace': 'widget',
-        'value': serialized,
-        'path': widget_guid,
-    }
-
-    return model
-
-
-def _create_widget_type_service_model(widget_type):
-    model = WidgetTypeSerializer(widget_type).data
-    del model['version']
-    return model
