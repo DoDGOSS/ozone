2863364b5bbf7c1652233ab1b536f4382fde19e2
William Ehrlich <william.d.ehrlich2.ctr@mail.mil>
2019-12-10 08:30:22 -0800

    DG-1318: Removed delete button from widget side bar

--- ozone-framework-client/packages/application/src/components/widget-toolbar/components/UserWidgetItem.tsx
+++ ozone-framework-client/packages/application/src/components/widget-toolbar/components/UserWidgetItem.tsx
@@ -1,17 +1,13 @@
 import styles from "../index.scss";
 
 import React, { useCallback, useState } from "react";
-import { Intent, Popover, PopoverInteractionKind, Position } from "@blueprintjs/core";
+import { Popover, PopoverInteractionKind, Position } from "@blueprintjs/core";
 
 import { assetUrl } from "../../../environment";
 import { useToggleable } from "../../../hooks";
 import { UserWidget } from "../../../models/UserWidget";
 import { dashboardService } from "../../../services/DashboardService";
-import { userWidgetService } from "../../../services/UserWidgetService";
-import { dashboardStore } from "../../../stores/DashboardStore";
 
-import { DeleteWidgetButton } from "./DeleteWidgetButton";
-import { showMarkdownDialog } from "./MarkdownConfirmationDialog";
 import { UserWidgetTile } from "./UserWidgetTile";
 
 export interface UserWidgetItemProps {
@@ -20,28 +16,13 @@ export interface UserWidgetItemProps {
 }
 
 const _UserWidgetItem: React.FC<UserWidgetItemProps> = ({ userWidget, widgetToolBarRef }) => {
-    const deleteButton = useToggleable(false);
     const [widgetToolBarMaxWidth, setWidgetToolBarMaxWidth] = useState(300);
 
     function handleOnMouseEnter() {
-        deleteButton.show();
         if (widgetToolBarRef && widgetToolBarRef.current) {
             setWidgetToolBarMaxWidth(widgetToolBarRef.current.clientWidth);
         }
     }
-
-    const onDelete = useCallback(() => {
-        userWidgetService
-            .deleteUserWidget(
-                userWidget,
-                showDeleteDependenciesConfirmation(userWidget),
-                showDeleteWidgetConfirmation(userWidget),
-                showGroupDependenciesFailureNotice(userWidget)
-            )
-            .then((isDeleted) => {
-                if (isDeleted) dashboardStore.fetchUserDashboards();
-            });
-    }, [userWidget]);
 
     const addWidget = useCallback(() => {
         dashboardService.addWidget({ widget: userWidget });
@@ -50,7 +31,7 @@ const _UserWidgetItem: React.FC<UserWidgetItemProps> = ({ userWidget, widgetTool
     const widget = userWidget.widget;
 
     return (
-        <div className={styles.tile} onMouseEnter={handleOnMouseEnter} onMouseLeave={deleteButton.hide}>
+        <div className={styles.tile} onMouseEnter={handleOnMouseEnter}>
             <div style={{ width: "100%" }}>
                 <Popover
                     interactionKind={PopoverInteractionKind.HOVER_TARGET_ONLY}
@@ -76,63 +57,8 @@ const _UserWidgetItem: React.FC<UserWidgetItemProps> = ({ userWidget, widgetTool
                     />
                 </Popover>
             </div>
-            <DeleteWidgetButton
-                isVisible={deleteButton.isVisible}
-                isGroupWidget={userWidget.isGroupWidget}
-                onClick={onDelete}
-            />
         </div>
     );
 };
 
 export const UserWidgetItem = React.memo(_UserWidgetItem);
-
-function showDeleteWidgetConfirmation(userWidget: UserWidget) {
-    return () => {
-        const widgetTitle = userWidget.widget.title;
-
-        return showMarkdownDialog({
-            title: "Warning",
-            text: `This action will permanently delete __${widgetTitle}__ from your available widgets!`,
-            confirmText: "Delete",
-            confirmIntent: Intent.DANGER
-        });
-    };
-}
-
-function showDeleteDependenciesConfirmation(userWidget: UserWidget) {
-    return (dependencies: UserWidget[]) => {
-        const widgetTitle = userWidget.widget.title;
-
-        let text = `This action will permanently delete __${widgetTitle}__ from your available widgets!\n\n`;
-        text += "The following dependent widgets will also be deleted:\n\n";
-        for (const dep of dependencies) {
-            text += `* __${dep.widget.title}__\n`;
-        }
-
-        return showMarkdownDialog({
-            title: "Warning",
-            text,
-            confirmText: "Delete",
-            confirmIntent: Intent.DANGER
-        });
-    };
-}
-
-function showGroupDependenciesFailureNotice(userWidget: UserWidget) {
-    return (dependencies: UserWidget[]) => {
-        const widgetTitle = userWidget.widget.title;
-
-        let text = `Unable to delete __${widgetTitle}__.\n\n`;
-        text += "The following dependent widgets were added by a group and may not be deleted:\n\n";
-        for (const dep of dependencies) {
-            text += `* __${dep.widget.title}__\n`;
-        }
-
-        return showMarkdownDialog({
-            title: "Error",
-            text,
-            cancelEnabled: false
-        });
-    };
-}
--- ozone-framework-client/packages/application/src/services/UserWidgetService.ts
+++ ozone-framework-client/packages/application/src/services/UserWidgetService.ts
@@ -1,21 +1,15 @@
-import { userWidgetApi, UserWidgetAPI } from "../api/clients/UserWidgetAPI";
-import { WidgetAPI, widgetApi } from "../api/clients/WidgetAPI";
-import { UserWidget } from "../models/UserWidget";
-import { DashboardStore, dashboardStore } from "../stores/DashboardStore";
+import { UserWidgetAPI } from "../api/clients/UserWidgetAPI";
+import { WidgetAPI } from "../api/clients/WidgetAPI";
+import { DashboardStore } from "../stores/DashboardStore";
 
 export class UserWidgetService {
-    // TODO: Multiple commmented lines in this file need to be uncommented once dependencies are re-implemented.
-    // private readonly widgetApi: WidgetAPI;
-    private readonly userWidgetApi: UserWidgetAPI;
     // private readonly dashboardStore: DashboardStore;
 
     constructor(_widgetApi?: WidgetAPI, _userWidgetApi?: UserWidgetAPI, _dashboardStore?: DashboardStore) {
-        // this.widgetApi = _widgetApi || widgetApi;
-        this.userWidgetApi = _userWidgetApi || userWidgetApi;
         // this.dashboardStore = _dashboardStore || dashboardStore;
     }
 
-    async getDependencies(userWidget: UserWidget): Promise<any> {
+    async getDependencies(): Promise<any> {
         // const dependentWidgets = await widgetApi.getDependentWidgets(userWidget.widget.id);
         // const dependentWidgetIds = dependentWidgets.data.data.map((w: any) => w.id);
         //
@@ -25,39 +19,6 @@ export class UserWidgetService {
         // return userWidgets.filter((w) => dependentWidgetIds.includes(w.widget.id));
         // return <any>[];
         return new Promise(() => undefined);
-    }
-
-    async deleteUserWidget(
-        userWidget: UserWidget,
-        confirmDeleteDependencies: (dependencies: UserWidget[]) => Promise<boolean>,
-        confirmDelete: () => Promise<boolean>,
-        onGroupDependencies: (dependencies: UserWidget[]) => void
-    ): Promise<boolean> {
-        const dependencies = await this.getDependencies(userWidget);
-
-        if (dependencies.length === 0) {
-            const deleteConfirmed = await confirmDelete();
-            if (!deleteConfirmed) return false;
-
-            await this.userWidgetApi.deleteUserWidget(userWidget.id);
-            return true;
-        }
-
-        const groupDependencies = dependencies.filter((w: any) => w.isGroupWidget);
-        if (groupDependencies.length > 0) {
-            onGroupDependencies(groupDependencies);
-            return false;
-        }
-
-        const deleteAllConfirmed = await confirmDeleteDependencies(dependencies);
-        if (!deleteAllConfirmed) return false;
-
-        for (const dependency of dependencies) {
-            await this.userWidgetApi.deleteUserWidget(dependency.widget.id);
-        }
-        // TODO: need to create an endpoint for deleting the PWD. A user deleting his own widget.
-        await this.userWidgetApi.deleteUserWidget(userWidget.id);
-        return true;
     }
 }
 
